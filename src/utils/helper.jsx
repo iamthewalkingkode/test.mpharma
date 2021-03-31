090e17102062ac24d666a790acdf14a0d011176b
iamTheWalkingKode <emmanuel@anchoratechs.com>
2021-03-31 12:27:25 +0000

    helper functions and icons

---
+++ src/utils/helper.jsx
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+const prefix = 'mpharma_';
+
+// :: axios get
+export const get = async (url, data = {}) => {
+    return axios({
+        method: 'GET', url,
+        headers: {},
+        params: data
+    }).then(response => {
+        const res = response.data;
+        return res;
+    }).catch(error => {
+        return { status: 606, result: 'Network request failed', error };
+    });
+}
+
+
+
+// ::: storage helpers
+export const setStorage = (key, value) => {
+    if (key && value) {
+        localStorage.setItem(prefix + key, value);
+    }
+}
+export const getStorage = (key) => {
+    const value = localStorage.getItem(prefix + key);
+    return value || '';
+}
+export const setStorageJson = (key, value) => {
+    if (key && value) {
+        localStorage.setItem(prefix + key, JSON.stringify(value));
+    }
+}
+export const getStorageJson = (key) => {
+    if (key) {
+        const value = localStorage.getItem(prefix + key);
+        return JSON.parse(value) || [];
+    }
+}
+export const delStorage = (key) => {
+    if (key) {
+        localStorage.removeItem(prefix + key);
+    }
+}
